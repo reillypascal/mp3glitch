c577e5ca88b3b8e1ad11a718b64410a5db6b8c47
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-15 23:32:47 -0400

    fixed file type test for zsh

--- batch.sh
+++ batch.sh
@@ -6,18 +6,17 @@
 # https://trac.ffmpeg.org/wiki/Encode/MP3
 
 for file in ./input/**/*(.); do
-	#if [ "${file##.}" == "wav" ]; then
+	# if wav, encode as mp3 vbr0
+	if [[ "${file##*.}" == "wav" ]]; then
 		ffmpeg -i $file -codec:a libmp3lame -q:a 0 ${file%.*}.mp3
-	#fi
-	
+	fi
+	# swap "input" w/ "output" in file path; make subdir in "output" folder
 	new_path=${file/input/output}
-	# new_ext=${new_path%.*}.wav
-	# mkdir -p $(dirname "${new_ext}")
 	mkdir -p $(dirname "${new_path}")
 	
 	python3 mp3glitch.py ${file%.*}.mp3 ${new_path%.*}.mp3 -M 10 -w 6	
-
+	# convert glitched mp3 back to wav
 	ffmpeg -i ${new_path%.*}.mp3 $new_path
-	
+	# remove both clean and glitched mp3s
 	rm ${file%.*}.mp3 ${new_path%.*}.mp3
 done
