62fe71a19e5f3ce219705e5acc7ba7bda7a545fe
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-16 20:18:30 -0400

    edited batch script

--- batch.sh
+++ batch.sh
@@ -8,15 +8,18 @@
 for file in ./input/**/*(.); do
 	# if wav, encode as mp3 vbr0
 	if [[ "${file##*.}" == "wav" ]]; then
-		ffmpeg -i $file -codec:a libmp3lame -q:a 0 ${file%.*}.mp3
+		# ffmpeg -i $file -codec:a libmp3lame -q:a 0 ${file%.*}.mp3
+		ffmpeg -i $file -codec:a libmp3lame -b:a 160k ${file%.*}.mp3
 	fi
 	# swap "input" w/ "output" in file path; make subdir in "output" folder
 	new_path=${file/input/output}
 	mkdir -p $(dirname "${new_path}")
 	
-	python3 mp3glitch.py ${file%.*}.mp3 ${new_path%.*}.mp3 -M 10 -w 6	
+	python3 mp3glitch.py ${file%.*}.mp3 ${new_path%.*}.mp3 #-p 3 -M 16 -w 8 -f 0
 	# convert glitched mp3 back to wav
 	ffmpeg -i ${new_path%.*}.mp3 $new_path
 	# remove both clean and glitched mp3s
 	rm ${file%.*}.mp3 ${new_path%.*}.mp3
+	# rm ${file%.*}.mp3
+
 done
